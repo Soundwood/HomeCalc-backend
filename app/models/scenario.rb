dacf43b127e3f8cd9320ed16e5449d6f2b41288c
Soundwood <j.benhogan@gmail.com>
2020-11-04 07:55:43 -0600

    add validations

--- app/models/scenario.rb
+++ app/models/scenario.rb
@@ -1,4 +1,14 @@
 class Scenario < ApplicationRecord
     belongs_to :prop_tax_rate
     belongs_to :mortg_interest_rate
+
+    validates :net_income, presence: true
+    validates :income_after_tax, presence: true
+    validates :monthly_debt, presence: true
+    validates :credit_score, presence: true
+    validates :downpayment, presence: true
+    validates :state, presence: true
+    validates :city, presence: true
+    validates :prop_tax_rate_id, presence: true
+    validates :mortg_interest_rate_id, presence: true
 end
