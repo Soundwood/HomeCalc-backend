d1d2f61b3b7adfec8b4e0b15b60033e292919039
Soundwood <j.benhogan@gmail.com>
2020-11-04 07:57:38 -0600

    limit routes to scenarios controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :mortg_interest_rates
-  resources :prop_tax_rates
+  # resources :mortg_interest_rates
+  # resources :prop_tax_rates
   resources :scenarios
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
