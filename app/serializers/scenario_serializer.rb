483e62c3c18d43acefa846d46f7c62a65d2d0987
Soundwood <j.benhogan@gmail.com>
2020-11-04 08:09:45 -0600

    add .env and serializer gems

---
+++ app/serializers/scenario_serializer.rb
@@ -0,0 +1,7 @@
+class ScenarioSerializer < ActiveModel::Serializer
+  attributes :id, :net_income, :income_after_tax, :monthly_debt, 
+  :credit_score, :downpayment, :state, :city
+
+  belongs_to :mortg_interest_rate
+  belongs_to :prop_tax_rate
+end
