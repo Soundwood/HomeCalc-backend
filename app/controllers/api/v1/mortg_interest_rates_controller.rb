16683f2f3ece7963766a0609741b5a02cc28775f
Soundwood <j.benhogan@gmail.com>
2020-11-08 17:58:49 -0600

    add index method

--- app/controllers/api/v1/mortg_interest_rates_controller.rb
+++ app/controllers/api/v1/mortg_interest_rates_controller.rb
@@ -1,2 +1,6 @@
 class Api::V1::MortgInterestRatesController < ApplicationController
+    def index
+        @mortg_interest_rates = MortgInterestRate.all
+        render json: @mortg_interest_rates
+    end
 end
--- app/controllers/api/v1/prop_tax_rates_controller.rb
+++ app/controllers/api/v1/prop_tax_rates_controller.rb
@@ -1,2 +1,6 @@
 class Api::V1::PropTaxRatesController < ApplicationController
+    def index
+        @prop_tax_rates = PropTaxRate.all
+        render json: @prop_tax_rates
+    end
 end
